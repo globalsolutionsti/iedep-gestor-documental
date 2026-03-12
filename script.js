b9a562b47ad9a53af3422d5d4240fbb216aba79d
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-12 00:05:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ e.preventDefault()
 let usuario = document.getElementById("usuario").value
 let pin = document.getElementById("pin").value
 
-// login simulado
+// simulación de validación
 if(usuario && pin){
 
+// guardar sesión
+localStorage.setItem("usuarioActivo", usuario)
+
+// redirigir al dashboard
 window.location.href = "dashboard.html"
 
 }
