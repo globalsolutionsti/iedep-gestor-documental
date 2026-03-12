fd3b409fd93384f27cdd30c70e11c83e0606ca22
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-12 00:06:05 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,27 @@
+// verificar sesión
 
+let usuario = localStorage.getItem("usuarioActivo")
+
+if(!usuario){
+
+window.location.href = "index.html"
+
+}
+
+// mostrar usuario en pantalla
+
+document.addEventListener("DOMContentLoaded", () => {
+
+document.getElementById("usuarioNombre").innerText = usuario
+
+})
+
+// cerrar sesión
+
+function cerrarSesion(){
+
+localStorage.removeItem("usuarioActivo")
+
+window.location.href = "index.html"
+
+}
