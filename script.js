7167e11a1af8722f1ec0abd15402d9bb42bea510
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-12 00:04:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ document.getElementById("loginForm").addEventListener("submit", function(e){
 
 e.preventDefault()
 
-// simulación de login correcto
+let usuario = document.getElementById("usuario").value
+let pin = document.getElementById("pin").value
+
+// login simulado
+if(usuario && pin){
+
 window.location.href = "dashboard.html"
+
+}
 
 })
